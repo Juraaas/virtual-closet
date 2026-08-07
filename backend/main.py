f7c0b95ec11ded551395845c4b53ad6ef12caa86
Juraaas <jurewiczjuras@gmail.com>
2026-08-08 00:18:03 +0200

    Register tryon router

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from api import wardrobe, users
+from api import wardrobe, users, tryon
 from services.storage import ensure_bucket_exists
 
 app = FastAPI(title="Virtual Closet API", version="0.1.0")
 
 app.include_router(wardrobe.router)
 app.include_router(users.router)
+app.include_router(tryon.router)
 
 @app.on_event("startup")
 def on_startup():
