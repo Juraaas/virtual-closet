d3e664e09cacf703808aebd4de6047378b53ab95
Juraaas <jurewiczjuras@gmail.com>
2026-08-08 10:41:18 +0200

    add cors for frontend and backend

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from api import wardrobe, users, tryon
 from services.storage import ensure_bucket_exists
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Virtual Closet API", version="0.1.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(wardrobe.router)
 app.include_router(users.router)
